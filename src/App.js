28445f884e107606780137c0b454ec0e6a048681
AMAN <antilaman3113@gmail.com>
2023-06-20 23:27:45 +0530

    initialized bookshelp app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./Components/HomePage";
+import SearchPage from "./Components/SearchPage";
+import ErrorElement from "./Components/ErrorElement";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="search" element={<SearchPage/>}/>
+      <Route path="*" element={<ErrorElement/>}/>
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
 
